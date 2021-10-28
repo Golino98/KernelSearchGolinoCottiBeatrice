ddf0e8c56ae0fbe3240f6ac841d27a515f25deb7
Golino98 <giacomogolino@gmail.com>
2021-10-28 14:03:45 +0200

    Modifica dello Start, aggiunta del path di una istanza

--- src/com/golino/cotti/classes/Start.java
+++ src/com/golino/cotti/classes/Start.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 public class Start {
     public static void main(String[] args) {
-        //String pathmps = ".\\MPS\\Instance1.mps";
-        String pathmps = args[0];
+
+        String pathtxt = ".\\istances\\SMALL\\probT1_0U_R50_T002_M010_N0020_seed01.txt";
+        //String pathmps = args[0];
         String pathlog = args[1];
         String pathConfig = args[2];
         Configuration config = ConfigurationReader.read(pathConfig);
-        KernelSearch ks = new KernelSearch(pathmps, pathlog, config);
+        KernelSearch ks = new KernelSearch(pathtxt, pathlog, config);
         ks.start();
 
         List<List<Double>> objValues = ks.getObjValues();
