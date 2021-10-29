b469c969bb253579cde0dbba05a29ab0a631f30f
Golino98 <giacomogolino@gmail.com>
2021-10-29 12:08:59 +0200

    Aggiunta classe Costanti.java e aggiunto controllo nella lettura del file contenente i dati per la configurazione iniziale

---
+++ src/com/golino/cotti/classes/Costanti.java
@@ -0,0 +1,13 @@
+package com.golino.cotti.classes;
+
+public class Costanti {
+
+    static final String ERRORE_KNAPSACK_CAPACITA = "Attenzione, non sono state definite tutte le capacità per i knapsacks.";
+    static final String ERRORE_KNAPSACK_VALORE = "Attenzione, è stata inserita una capacità knapsack negativa o uguale a zero.";
+
+    static final String ERRORE_ITEM_PESO = "Attenzione, non sono stati definiti tutti i pesi degli items.";
+    static final String ERRORE_ITEM_PESO_VALORE = "Attenzione, è stata inserita un peso di un item negativo o uguale a zero.";
+
+    static final String ERRORE_ITEM_PROFITTO = "Attenzione, non sono stati definiti tutti i profitti degli items.";
+    static final String ERRORE_ITEM_PROFITTO_VALORE = "Attenzione, è stata inserita un profitto di un item negativo o uguale a zero.";
+}
--- src/com/golino/cotti/classes/Model.java
+++ src/com/golino/cotti/classes/Model.java
@@ -5,6 +5,7 @@ import gurobi.GRB.DoubleAttr;
 import gurobi.GRB.IntAttr;
 import gurobi.GRB.StringAttr;
 
+import javax.management.InvalidAttributeValueException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static com.golino.cotti.classes.Costanti.*;
 
 public class Model {
     private final String mpsFilePath;
@@ -59,26 +62,55 @@ public class Model {
             int number_of_knapsacks = Integer.parseInt(lines.get(0));
             int number_of_items = Integer.parseInt(lines.get(1));
 
-            //for che legge tutte le capacità dei knapsack
+            /**
+             * Il seguente ciclo va a suddividere la {@link List<String>} in una sottostringa, prendendo come primo riferimento
+             * l'elemento in posizione due della lista (capacità del primo knapsack) e come ultimo riferimento l'ultima capacità
+             * dell'ultimo knapsack, presente in posizione 2 + (numero dei knapsack presenti).
+             * Si va a sommare il valore due in quanto le prime due righe del file contenente i dati contengono il numero dei
+             * knapsacks e degli items.
+             * Inoltre viene effettuato un controllo di correttezza dei dati, ovvero, devono essere stati inseriti tutti e devono
+             * essere tutti maggiori di zero
+             */
+
             for (String line : lines.subList(2, 2+number_of_knapsacks))
             {
-                throw new IOException("Errore, il file delle istanze utilizzato non contiene tutte le capacità dei knapsack");
-            }
-
-            //for che passa tutte le righe contenenti peso e profitto di ogni item
-            for (String line : lines.subList(number_of_knapsacks, number_of_items + number_of_knapsacks))
-            {
-                String[] splitLine = line.split("\\s+");
-                if(splitLine[0].isBlank() || splitLine[1].isBlank())
+                if(line.isBlank())
                 {
-                    throw new IOException("Errore, il file delle istanze utilizzato non contiene tutti i pesi o i profitti degli item");
-                }else
+                    throw new IOException(ERRORE_KNAPSACK_CAPACITA);
+                }else if(Integer.parseInt(line)<=0)
                 {
-                    //Andare a settare gli item con il loro peso e profitto
+                    throw new InvalidAttributeValueException(ERRORE_KNAPSACK_VALORE);
                 }
             }
 
-        } catch (IOException e) {
+            /**
+             * Il seguente ciclo va a suddividere la {@link List<String>} in una sottostringa, prendendo come primo riferimento
+             * il numero dei knapsacks presenti e sommandogli due (in quanto le prime due righe sono utilizzate per la definizione
+             * del numero di knapsacks e di items) e come ultimo riferimento l'elemento in posizione finale, ovvero quella
+             * corrispondente al numero di item sommata al numero dei knapsack e a due (prime due righe per la definizione del numero
+             * di knapsacks e items).
+             * Inoltre viene effettuato un controllo di correttezza dei dati, ovvero, devono essere stati inseriti tutti e devono
+             * essere tutti maggiori di zero
+             */
+            for (String line : lines.subList(number_of_knapsacks + 2, number_of_items + number_of_knapsacks + 2 ))
+            {
+                String[] splitLine = line.split("\\s+");
+                if(splitLine[0].isBlank())
+                {
+                    throw new IOException(ERRORE_ITEM_PESO);
+                }else if(Integer.parseInt(splitLine[0])<=0)
+                {
+                    throw new InvalidAttributeValueException(ERRORE_ITEM_PESO_VALORE);
+                }else if(splitLine[1].isBlank())
+                {
+                    throw new IOException(ERRORE_ITEM_PROFITTO);
+                }else if (Integer.parseInt(splitLine[1])<=0)
+                {
+                    throw new InvalidAttributeValueException(ERRORE_ITEM_PESO_VALORE);
+                }
+            }
+
+        } catch (IOException | InvalidAttributeValueException e) {
             e.printStackTrace();
         }
 
@@ -89,10 +121,10 @@ public class Model {
         env.set(GRB.IntParam.Threads, config.getNumThreads());
         env.set(GRB.IntParam.Presolve, config.getPresolve());
         env.set(GRB.DoubleParam.MIPGap, config.getMipGap());
-        if (timeLimit > 0)
+        if (timeLimit > 0) {
             env.set(GRB.DoubleParam.TimeLimit, timeLimit);
+        }
         //env.set(GRB.IntParam.Method, 0);
-        //env.set(GRB.IntParam.Presolve, 0);
         //env.set(IntParam.OutputFlag, 0);
 
     }
