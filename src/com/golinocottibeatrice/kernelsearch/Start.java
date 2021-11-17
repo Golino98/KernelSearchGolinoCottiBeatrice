2dfdb6f9fff6c4bc3c2bea4ee2888f5fb988b361
Golino98 <giacomogolino@gmail.com>
2021-11-17 11:47:48 +0100

    Modifica del gitignore per la modifica del config.default.txt

--- src/com/golinocottibeatrice/kernelsearch/Start.java
+++ src/com/golinocottibeatrice/kernelsearch/Start.java
@@ -8,24 +8,22 @@ public class Start {
 
     // Path per il file di configurazione (in cui sono presenti tutti i dati per il
     // settaggio del numero di bucket e così via)
-    private static final String defaultConfigPath = "./config.txt";
+    private static final String defaultConfigPath = "./config.default.txt";
 
     //TODO Modificare in modo che non ci sia più il ciclo in questo modo ma che legga tutti i file presenti in una cartella automaticamente
     public static void main(String[] args) {
-        for (int i = 1; i <= 480; i++) {
-            String configPath = defaultConfigPath;
-            if (args.length != 0 && !args[0].isEmpty()) {
-                configPath = args[0];
-            }
-
-            try {
-                var config = new ConfigurationReader(configPath).read(i);
-                KernelSearch ks = new KernelSearch(config);
-                ks.start();
-            } catch (IOException | GRBException e) {
-                e.printStackTrace();
-
-            }
+        String configPath = defaultConfigPath;
+        if (args.length != 0 && !args[0].isEmpty()) {
+            configPath = args[0];
         }
+
+        try {
+            var config = new ConfigurationReader(configPath).read();
+            KernelSearch ks = new KernelSearch(config);
+            ks.start();
+        } catch (IOException | GRBException e) {
+            e.printStackTrace();
+        }
+
     }
 }
