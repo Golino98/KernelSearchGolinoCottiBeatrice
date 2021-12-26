0199067bd28c5d17e5154f7a51d69a3c2e92260b
Luca Cotti <lucacotti@outlook.com>
2021-12-26 17:26:24 +0100

    Refactoring di Variable

--- src/main/java/com/golinocottibeatrice/kernelsearch/solver/Variable.java
+++ src/main/java/com/golinocottibeatrice/kernelsearch/solver/Variable.java
@@ -1,28 +1,19 @@
 package com.golinocottibeatrice.kernelsearch.solver;
 
+import com.golinocottibeatrice.kernelsearch.instance.Item;
+
 /**
  * Rappresenta una variabile x(i,j) del problema.
  */
 public class Variable {
     private final String name;
-    private final double value;
-    private final double rc;
-    private final int weight;
-    private final int profit;
+    private final Item item;
+    private double value;
+    private double rc;
 
-    /**
-     * Crea una nuova variabile.
-     *
-     * @param name  Il nome della variabile.
-     * @param value Il valore assegnato alla variabile (non necessariamente binario).
-     * @param rc    Il valore del costo ridotto (diverso da zero solo se rilassamento continuo).
-     */
-    Variable(String name, double value, double rc, int weight, int profit) {
+    Variable(String name, Item item) {
         this.name = name;
-        this.value = value;
-        this.rc = rc;
-        this.weight = weight;
-        this.profit = profit;
+        this.item = item;
     }
 
     public String getName() {
@@ -34,28 +25,26 @@ public class Variable {
     }
 
     public int getWeight() {
-        return weight;
+        return item.getWeight();
     }
 
     public int getProfit() {
-        return profit;
+        return item.getProfit();
     }
 
-
-    /**
-     * Restituisce il valore del costo ridotto assegnato alla variabile.
-     *
-     * @return Il costo ridotto, diverso da zero solo se rilassamento continuo.
-     */
     public double getRC() {
         return rc;
     }
 
-    /**
-     * Restituisce il valore del profit/weight (utilizzato nella classe VariableSorterByProfitDivideWeight
-     * @return profit/weight
-     */
     public double getProfitDividedByWeight() {
-        return ((double)this.profit) / ((double)this.weight);
+        return ((double) getProfit()) / ((double) getWeight());
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setRc(double rc) {
+        this.rc = rc;
     }
 }
