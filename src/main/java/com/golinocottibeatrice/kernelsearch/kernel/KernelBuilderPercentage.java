3a017f9ece60f5a6dc549a1a3825be99e557ef29
Golino98 <giacomogolino@gmail.com>
2022-05-26 15:46:19 +0200

    Aggounta commenti funzionamento per spiegazione tesi

--- src/main/java/com/golinocottibeatrice/kernelsearch/kernel/KernelBuilderPercentage.java
+++ src/main/java/com/golinocottibeatrice/kernelsearch/kernel/KernelBuilderPercentage.java
@@ -9,14 +9,24 @@ public class KernelBuilderPercentage implements KernelBuilder {
     @Override
     public Kernel build(List<Variable> variables, SearchConfiguration config) {
 
+        //Creazione dell'insieme Kernel che verra' in cui verranno aggiunte le variabili
         Kernel kernel = new Kernel();
-        for (var v : variables) {
-            if (kernel.size() < Math.round(config.getKernelSize() * variables.size())) {
+
+        //Ciclo "foreach" per effettuare il controllo su tutte le variabili del problema
+        for (var v : variables)
+        {
+            //Controllo della condizione sulla dimenzione del Kernel
+            if (kernel.size() < Math.round(config.getKernelSize() * variables.size()))
+            {
+                //Condizione verificata, aggiunta della variabile v nel Kernel
                 kernel.addItem(v);
-            } else {
+            } else
+            {
+                //Condizione non verificata. Break per passare alla prossima variabile
                 break;
             }
         }
+        //Viene restituito il Kernel in cui sono state inserite le variabili che soddisfano la condizione
         return kernel;
     }
 }
