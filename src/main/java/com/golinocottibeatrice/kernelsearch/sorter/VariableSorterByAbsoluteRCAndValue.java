91c0b5cbf3749df8109d45af7f69012c44932c40
Golino98 <giacomogolino@gmail.com>
2022-06-24 10:49:15 +0200

    Modifica di un ordered

--- src/main/java/com/golinocottibeatrice/kernelsearch/sorter/VariableSorterByAbsoluteRCAndValue.java
+++ src/main/java/com/golinocottibeatrice/kernelsearch/sorter/VariableSorterByAbsoluteRCAndValue.java
@@ -6,6 +6,9 @@ import java.util.Comparator;
 import java.util.List;
 
 public class VariableSorterByAbsoluteRCAndValue implements VariableSorter {
+
+    //Potrebbe essere meglio fare
+    //variables.sort(Comparator.comparing(Variable::getRC).reversed.thenComparing(Variable::getValue));
     @Override
     public void sort(List<Variable> variables) {
         variables.sort(Comparator.comparing(Variable::getRC).thenComparing(Variable::getValue).reversed());
